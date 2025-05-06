80ab7c255a82daf2c119654c3ba15ffe4e8dcf35
Mehdi <muhammad-ahsan.mehdi.ext@siemens.com>
2025-05-06 18:14:45 +0500

    Add TC11, upload a file with file name 255 chars long

--- tests/file-upload.spec.ts
+++ tests/file-upload.spec.ts
@@ -3,6 +3,7 @@
 */
 import { test, expect } from '@playwright/test';
 import path from 'path';
+import * as fs from 'fs/promises';
 /*
 -- import the custome utility function to generate the random string
 */
@@ -23,21 +24,14 @@ TC001
 test('user should be able to upload a pdf file with 19kb size', async () => {
   // user should be on the website url
   await expect(website.locator("//h3[contains(text(),'File Uploader')]")).toHaveText('File Uploader');
-
   // current directory and the path
   const current_dir = process.cwd();
-  console.log('Current Directory:', current_dir);
-
   // designing the file path
   const file_path = path.join(current_dir, 'tests', 'Sample Data Files', 'PDF File Size-19KB.pdf');
-  console.log('File Path:', file_path);
-
   // upload  file
   await website.locator('#file-upload').setInputFiles(file_path);
-
   // Click  upload button
   await website.locator("//input[@value='Upload']").click();
-
   // Add the assertion
   await expect(website.locator('#uploaded-files')).toHaveText('PDF File Size-19KB.pdf');
 });
@@ -49,22 +43,15 @@ TC002
 test('user should be able to upload a word file with 14kb size', async () => {
   // Verify the expected page has loaded
   await expect(website.locator("//h3[contains(text(),'File Uploader')]")).toHaveText('File Uploader');
-
   // Find the current directory and design the file path
   const current_dir = process.cwd();
-  console.log('Current Directory:', current_dir);
-
-  // Assuming "tests" is the root folder, adjust file path accordingly
+  // Tests folder contains the sample data files folder
   const file_path = path.join(current_dir, 'tests', 'Sample Data Files', 'Word File Size-14KB.docx');
-  console.log('File Path:', file_path);
-
   // Step 3: Upload the file
   await website.locator('#file-upload').setInputFiles(file_path);
-
   // Step 4: Click the upload button
   await website.locator("//input[@value='Upload']").click();
-
-  // Optionally: Add some verification after the upload
+  // Add assertion
   await expect(website.locator('#uploaded-files')).toHaveText('Word File Size-14KB.docx');
 
 });
@@ -75,21 +62,14 @@ test('user should be able to upload a word file with 14kb size', async () => {
 test ('user should be able to upload a jpg file of size 0.98MB', async () => {
   // user should be on the website url
   await expect(website.locator("//h3[contains(text(),'File Uploader')]")).toHaveText('File Uploader');
-
   // current directory and the path
   const current_dir = process.cwd();
-  console.log('Current Directory:', current_dir);
-
   // designing the file path
   const file_path = path.join(current_dir, 'tests', 'Sample Data Files', 'Image file _.098 MB.jpg');
-  console.log('File Path:', file_path);
-
   // upload  file
   await website.locator('#file-upload').setInputFiles(file_path);
-
   // Click  upload button
   await website.locator("//input[@value='Upload']").click();
-
   // Add the assertion
   await expect(website.locator('#uploaded-files')).toHaveText('Image file _.098 MB.jpg');
 });
@@ -101,21 +81,14 @@ test ('user should be able to upload a jpg file of size 0.98MB', async () => {
 test ('user should be able to upload a MP4 file of size 13 MB', async () => {
   // user should be on the website url
   await expect(website.locator("//h3[contains(text(),'File Uploader')]")).toHaveText('File Uploader');
-
   // current directory and the path
   const current_dir = process.cwd();
-  console.log('Current Directory:', current_dir);
-
   // designing the file path
   const file_path = path.join(current_dir, 'tests', 'Sample Data Files', 'MP4 file Size-13MB.mp4');
-  console.log('File Path:', file_path);
-
   // upload  file
   await website.locator('#file-upload').setInputFiles(file_path);
-
   // Click  upload button
   await website.locator("//input[@value='Upload']").click();
-
   // Add the assertion
   await expect(website.locator('#uploaded-files')).toHaveText('MP4 file Size-13MB.mp4');
 });
@@ -126,21 +99,14 @@ test ('user should be able to upload a MP4 file of size 13 MB', async () => {
 test ('user should be able to upload an excel file of size 10 KB', async () => {
   // user should be on the website url
   await expect(website.locator("//h3[contains(text(),'File Uploader')]")).toHaveText('File Uploader');
-
   // current directory and the path
   const current_dir = process.cwd();
-  console.log('Current Directory:', current_dir);
-
   // designing the file path
   const file_path = path.join(current_dir, 'tests', 'Sample Data Files', 'Excel.xlsx');
-  console.log('File Path:', file_path);
-
   // upload  file
   await website.locator('#file-upload').setInputFiles(file_path);
-
   // Click  upload button
   await website.locator("//input[@value='Upload']").click();
-
   // Add the assertion
   await expect(website.locator('#uploaded-files')).toHaveText('Excel.xlsx');
 });
@@ -152,21 +118,14 @@ test ('user should be able to upload an excel file of size 10 KB', async () => {
 test ('user should be able to upload a TXT file of size 1 KB', async () => {
   // user should be on the website url
   await expect(website.locator("//h3[contains(text(),'File Uploader')]")).toHaveText('File Uploader');
-
   // current directory and the path
   const current_dir = process.cwd();
-  console.log('Current Directory:', current_dir);
-
   // designing the file path
   const file_path = path.join(current_dir, 'tests', 'Sample Data Files', 'TXT File.txt');
-  console.log('File Path:', file_path);
-
   // upload  file
   await website.locator('#file-upload').setInputFiles(file_path);
-
   // Click  upload button
   await website.locator("//input[@value='Upload']").click();
-
   // Add the assertion
   await expect(website.locator('#uploaded-files')).toHaveText('TXT File.txt');
 });
@@ -178,21 +137,14 @@ test ('user should be able to upload a TXT file of size 1 KB', async () => {
 test ('user should be able to upload a TXT file of size 0 KB', async () => {
   // user should be on the website url
   await expect(website.locator("//h3[contains(text(),'File Uploader')]")).toHaveText('File Uploader');
-
   // current directory and the path
   const current_dir = process.cwd();
-  console.log('Current Directory:', current_dir);
-
   // designing the file path
   const file_path = path.join(current_dir, 'tests', 'Sample Data Files', 'Empty File.txt');
-  console.log('File Path:', file_path);
-
   // upload  file
   await website.locator('#file-upload').setInputFiles(file_path);
-
   // Click  upload button
   await website.locator("//input[@value='Upload']").click();
-
   // Add the assertion
   await expect(website.locator('#uploaded-files')).toHaveText('Empty File.txt');
 });
@@ -204,21 +156,14 @@ test ('user should be able to upload a TXT file of size 0 KB', async () => {
 test ('user should be able to upload a dot exe file', async () => {
   // user should be on the website url
   await expect(website.locator("//h3[contains(text(),'File Uploader')]")).toHaveText('File Uploader');
-
   // current directory and the path
   const current_dir = process.cwd();
-  console.log('Current Directory:', current_dir);
-
   // designing the file path
   const file_path = path.join(current_dir, 'tests', 'Sample Data Files', 'Dot EXE.exe');
-  console.log('File Path:', file_path);
-
   // upload  file
   await website.locator('#file-upload').setInputFiles(file_path);
-
   // Click  upload button
   await website.locator("//input[@value='Upload']").click();
-
   // Add the assertion
   await expect(website.locator('#uploaded-files')).toHaveText('Dot EXE.exe');
 });
@@ -231,21 +176,14 @@ test ('user should be able to upload a dot exe file', async () => {
 test ('user should be able to upload a large file of size 70MB', async () => {
   // user should be on the website url
   await expect(website.locator("//h3[contains(text(),'File Uploader')]")).toHaveText('File Uploader');
-
   // current directory and the path
   const current_dir = process.cwd();
-  console.log('Current Directory:', current_dir);
-
   // designing the file path
   const file_path = path.join(current_dir, 'tests', 'Sample Data Files', 'Large File 70MB.mp4');
-  console.log('File Path:', file_path);
-
   // upload  file
   await website.locator('#file-upload').setInputFiles(file_path);
-
   // Click  upload button
   await website.locator("//input[@value='Upload']").click();
-
   // Add the assertion
   await expect(website.locator('#uploaded-files')).toHaveText('Large File 70MB.mp4');
 });
@@ -258,22 +196,47 @@ test ('user should be able to upload a large file of size 70MB', async () => {
 test ('user should be able to upload a Zip file', async () => {
   // user should be on the website url
   await expect(website.locator("//h3[contains(text(),'File Uploader')]")).toHaveText('File Uploader');
-
   // current directory and the path
   const current_dir = process.cwd();
-  console.log('Current Directory:', current_dir);
-
   // designing the file path
   const file_path = path.join(current_dir, 'tests', 'Sample Data Files', 'ZIP FILE.zip');
-  console.log('File Path:', file_path);
-
   // upload  file
   await website.locator('#file-upload').setInputFiles(file_path);
+  // Click  upload button
+  await website.locator("//input[@value='Upload']").click();
+  // Add the assertion
+  await expect(website.locator('#uploaded-files')).toHaveText('ZIP FILE.zip');
+});
+
+
+/*
+ TC11
+*/
+test ('user should be able to upload a file having 255 chars in file name including the extension', async () => {
+  // user should be on the website url
+  await expect(website.locator("//h3[contains(text(),'File Uploader')]")).toHaveText('File Uploader');
+  // current directory and the path
+  const current_dir = process.cwd();
+  // designing the file path
+  const file_path = path.join(current_dir, 'tests', 'Sample Data Files');
+  // generate a random string
+  const long_string = random_string_gen(251);
+  const new_filename = `${long_string}.txt`;
+
+  const old_file = path.join(file_path, 'Random.txt');
+  const new_file = path.join(file_path, new_filename);
+
+  // Rename the file
+  await fs.rename(old_file, new_file);
+
+  console.log(`Renamed file to: ${new_file}`);
+
+  // upload  file
+  await website.locator('#file-upload').setInputFiles(new_file);
 
   // Click  upload button
   await website.locator("//input[@value='Upload']").click();
 
   // Add the assertion
-  await expect(website.locator('#uploaded-files')).toHaveText('ZIP FILE.zip');
+  await expect(website.locator('#uploaded-files')).toHaveText(new_filename);
 });
-
--- utils/dynamic_data_gen.ts
+++ utils/dynamic_data_gen.ts
@@ -1,16 +1,15 @@
 /*
  -- function generate a random string of length provided by tester
 */
-function random_string_gen(length: number): string 
+export function random_string_gen(length: number): string 
 {   // set a string which contains the upper and lower case letters along with numbers
     const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let result = '';
+    var result = '';
     // generating a string
     for (let i = 0; i < length; i++) {
       const random_index = Math.floor(Math.random() * chars.length);
       result += chars[random_index];
     }
-  
     return result;
 }
   
